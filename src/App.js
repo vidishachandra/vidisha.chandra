14138fe7da21bc46e8b114b309a7bde40d5182cb
vidishachandra <vidishasateesh@gmail.com>
2024-08-17 11:52:49 -0500

    Updated profile

--- src/App.js
+++ src/App.js
@@ -1,4 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Header from "./Components/Header";
+// import Footer from "./Components/Footer";
+// import Home from "./Pages/Home";
+// import About from "./Pages/About";
+// import Contact from "./Pages/Contact";
+// import Projects from "./Pages/Projects";
+// import Technologies from "./Pages/Technologies";
+
+// function App() {
+//   return (
+//     <Router basename="/">
+//       <Header />
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/contact" element={<Contact />} />
+//         <Route path="/projects" element={<Projects />} />
+//         <Route path="/technologies" element={<Technologies />} />
+//         <Route path="*" element={<Home />} /> {/* Redirect all unknown paths to Home */}
+
+//       </Routes>
+//       <Footer />
+//     </Router>
+//   );
+// }
+
+// export default App;
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Home from "./Pages/Home";
@@ -9,7 +37,7 @@ import Technologies from "./Pages/Technologies";
 
 function App() {
   return (
-    <Router basename="/">
+    <Router>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -17,8 +45,6 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/technologies" element={<Technologies />} />
-        <Route path="*" element={<Home />} /> {/* Redirect all unknown paths to Home */}
-
       </Routes>
       <Footer />
     </Router>
